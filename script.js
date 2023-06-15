2d4512eb38521b4b98fa012fad9086aed7ad45e5
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2023-06-15 07:14:16 +0530

    3 files

---
+++ script.js
@@ -0,0 +1,8 @@
+burger = document.querySelector('.burger')
+rightnav = document.querySelector('.right-nav')
+leftnav = document.querySelector('.left-nav')
+navbar = document.querySelector('.navbar')
+burger.addEventListner('click',()=>{
+    rightnav.classList.toggle('.visibility-class')
+    rightnav.classList.toggle('.visibility-class')
+})
